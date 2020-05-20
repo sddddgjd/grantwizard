b9c0273244e147387c2646eba59c7262763d14a5
Rebecca Yip <rebecca.yip@Rebz-Klook-Mac.local>
2020-05-20 21:53:50 +0800

    hi A

--- grantobj.js
+++ grantobj.js
@@ -44,18 +44,18 @@ let grantObj = {
             //should we actually put value type 'monthly' or just the id?
             value_type: 'monthly'
             eligibility_requirements: [{
-                    req_id: 2001,
-                    name: 'Hong Kong Residency',
-                    question: 'Have you been an HKID cardholder for more than 12 months?',
+                    req_id: 2003,
+                    name: 'TITLE',
+                    question: 'QUESTION?',
                     //should we actually put question type 'plain text' or just the id?
                     question_type: 101,
                     //should we actually put validation type 'binary' or just the id?
                     validation_type_id: 14},
                 {
                     req_id: 2002,
-                    name: 'monthly working hours',
-                    question: 'some question here',
-                    //should we actually put question type 'multiple choice' or just the id?
+                    name: 'Household asset value',
+                    question: 'What is the total value of your household assets?',
+                    //should we actually put question type 'plain text' or just the id?
                     question_type: 104,
                     //should we actually put validation type 'integer' or just the id?
                     validation_type_id: 10}
