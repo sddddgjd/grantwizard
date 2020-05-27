45d62f0fd3349ae34f00b94dbb0c0a0f3b6c9625
Anasstasia Zloteanu <zlot.anastasia@gmail.com>
2020-05-27 14:15:47 +0300

    Updates

---
+++ precache-manifest.595efeb53ea5824fbaadd1f5579ac05a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309c5f1a9d2b99463c35e431b2813ab1",
+    "url": "/grantwizard/index.html"
+  },
+  {
+    "revision": "14b976e5aee081d6c58e",
+    "url": "/grantwizard/static/css/main.8ef78ab6.chunk.css"
+  },
+  {
+    "revision": "a09c5c5f9ac9c27e892b",
+    "url": "/grantwizard/static/js/2.041c5145.chunk.js"
+  },
+  {
+    "revision": "14b976e5aee081d6c58e",
+    "url": "/grantwizard/static/js/main.21297f40.chunk.js"
+  },
+  {
+    "revision": "5fe56525de032247c5f3",
+    "url": "/grantwizard/static/js/runtime~main.4e5b4fea.js"
+  },
+  {
+    "revision": "91f53c3fcbd45abf248940f1a524ed49",
+    "url": "/grantwizard/static/media/auto-apply.91f53c3f.svg"
+  },
+  {
+    "revision": "828f76d1b7f7abf4fc856945fe21c857",
+    "url": "/grantwizard/static/media/background.828f76d1.svg"
+  },
+  {
+    "revision": "dd0f41553c61197683e37532fce55dff",
+    "url": "/grantwizard/static/media/find-jobs.dd0f4155.svg"
+  },
+  {
+    "revision": "dd49314cb01712a70eef6f9d46550e09",
+    "url": "/grantwizard/static/media/remote-work-woman.dd49314c.png"
+  },
+  {
+    "revision": "9bd2340fea2ffc62935fd57f77822a1a",
+    "url": "/grantwizard/static/media/step-by-step.9bd2340f.svg"
+  }
+]);
